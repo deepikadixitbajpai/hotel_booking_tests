1f56736f33755cc6985afa0b87052dec2cc3f47a
Deepika Dixit <deepika.dixit@bglgroup.co.uk>
2019-02-06 13:38:46 +0000

    Add linting and update delete function to handle exception

--- features/step-definitions/hotel_bookings_steps.js
+++ features/step-definitions/hotel_bookings_steps.js
@@ -1,3 +1,6 @@
+/* eslint prefer-destructuring: 0 */
+/* eslint no-undef: 0 */
+
 const expect = require('chai').expect;
 const uuidv4 = require('uuid/v4');
 
@@ -5,34 +8,34 @@ const hotelBookingPage = require('../../page_objects/hotel_booking.page');
 const { Given, When, Then } = require('cucumber');
 
 Given(/^I am on hotel booking page$/, () => {
-   hotelBookingPage.open();
-   expect(hotelBookingPage.getHeading()).to.eql('Hotel booking form');
+  hotelBookingPage.open();
+  expect(hotelBookingPage.getHeading()).to.eql('Hotel booking form');
 });
 
-Given(/^I enter the following details in the form:$/, (table) => {
-   customerDetails = table.hashes();
-   for(var i in customerDetails) {
-      customerDetails[i].firstname = customerDetails[i].firstname + uuidv4();
-      hotelBookingPage.fillBookingForm(customerDetails[i]);
-   }
+Given(/^I enter the following details in the form:$/, table => {
+  customerDetails = table.hashes();
+  for (const i in customerDetails) {
+    customerDetails[i].firstname = customerDetails[i].firstname + uuidv4();
+    hotelBookingPage.fillBookingForm(customerDetails[i]);
+  }
 });
 
 When(/^I save the booking$/, () => {
-   savedBooking = hotelBookingPage.saveBooking(customerDetails[0].firstname);
+  savedBooking = hotelBookingPage.saveBooking(customerDetails[0].firstname);
 });
 
 Then(/^my booking record should be displayed on the page$/, () => {
-   expect(savedBooking.firstName).to.eql(customerDetails[0].firstname);
-   expect(savedBooking.price).to.eql(customerDetails[0].price);
-   expect(savedBooking.depositPaid).to.eql(customerDetails[0].deposit);
-   expect(savedBooking.checkinDate).to.eql(customerDetails[0].checkin);
-   expect(savedBooking.checkoutDate).to.eql(customerDetails[0].checkout);
+  expect(savedBooking.firstName).to.eql(customerDetails[0].firstname);
+  expect(savedBooking.price).to.eql(customerDetails[0].price);
+  expect(savedBooking.depositPaid).to.eql(customerDetails[0].deposit);
+  expect(savedBooking.checkinDate).to.eql(customerDetails[0].checkin);
+  expect(savedBooking.checkoutDate).to.eql(customerDetails[0].checkout);
 });
 
 Then(/^I click the delete button for my booking$/, () => {
-   hotelBookingPage.deleteBooking(savedBooking.bookingId);
+  hotelBookingPage.deleteBooking(savedBooking.bookingId);
 });
 
 Then(/^the booking entry should no longer be displayed on the page$/, () => {
-   expect(hotelBookingPage.findBookingByFirstname(savedBooking.firstName)).to.not.exist;
+  expect(hotelBookingPage.findBookingByBookingId(savedBooking.bookingId)).to.eql(false);
 });
--- page_objects/hotel_booking.page.js
+++ page_objects/hotel_booking.page.js
@@ -1,8 +1,7 @@
 module.exports = {
+  getHeading: () => browser.getTitle(),
 
-  getHeading: () => { return browser.getTitle(); },
-
-  fillBookingForm: (customerDetails) => {
+  fillBookingForm: customerDetails => {
     browser.element('#firstname').setValue(customerDetails.firstname);
     browser.element('#lastname').setValue(customerDetails.lastname);
     browser.element('#totalprice').setValue(customerDetails.price);
@@ -12,46 +11,77 @@ module.exports = {
   },
 
   bookings: () => {
-    let allRows = browser.elements('#bookings div.row')
+    const allRows = browser.elements('#bookings div.row');
     allRows.value.shift();
     return allRows.value;
   },
 
   saveBooking: firstName => {
-    console.log("customerName:", firstName)
+    console.log('customerName:', firstName);
     browser.element('input[value=" Save "]').click();
     let savedBooking;
-    let bookingObject;
-    browser.waitUntil(() => {
-      let booking = module.exports.findBookingByFirstname(firstName);
-      if(booking != null) {
-         savedBooking = booking
-        return true;
-      }
-      return false;
-    }, 10000, 'expected bookings count to be saved in 10s');
-    bookingObject = {bookingId: savedBooking.getAttribute('id'),
-                    firstName: savedBooking.element('div:nth-child(1) p').getText(),
-                    lastName: savedBooking.element('div:nth-child(2) p').getText(), 
-                    price: savedBooking.element('div:nth-child(3) p').getText(),
-                    depositPaid: savedBooking.element('div:nth-child(4) p').getText(),
-                    checkinDate: savedBooking.element('div:nth-child(5) p').getText(),
-                    checkoutDate: savedBooking.element('div:nth-child(6) p').getText()
-                  };
-                  console.log("saved booking:",bookingObject)
+    browser.waitUntil(
+      () => {
+        const booking = module.exports.findBookingByFirstname(firstName);
+        if (booking != null) {
+          savedBooking = booking;
+          return true;
+        }
+        return false;
+      },
+      browser.options.waitforTimeout,
+      'expected bookings count to be saved in 10s',
+    );
+
+    const bookingObject = {
+      bookingId: savedBooking.getAttribute('id'),
+      firstName: savedBooking.element('div:nth-child(1) p').getText(),
+      lastName: savedBooking.element('div:nth-child(2) p').getText(),
+      price: savedBooking.element('div:nth-child(3) p').getText(),
+      depositPaid: savedBooking.element('div:nth-child(4) p').getText(),
+      checkinDate: savedBooking.element('div:nth-child(5) p').getText(),
+      checkoutDate: savedBooking.element('div:nth-child(6) p').getText(),
+    };
     return bookingObject;
   },
 
   deleteBooking: bookingId => {
-    module.exports.findBookingByBookingId(bookingId).$('input[value="Delete"]').click();
+    module.exports
+      .findBookingByBookingId(bookingId)
+      .$('input[value="Delete"]')
+      .click();
+    // browser.pause(5000)
+    browser.waitUntil(
+      () => {
+        const booking = module.exports.findBookingByBookingId(bookingId);
+        console.log('what is booking status:', booking);
+        if (booking === false) {
+          return true;
+        }
+        return false;
+      },
+      browser.options.waitforTimeout,
+      'expected booking to be deleted in 10s',
+    );
   },
 
-  findBookingByFirstname: firstname => {
-    return module.exports.bookings().find(booking => booking.element('div:nth-child(1) p').getText() === firstname);
-  },
+  findBookingByFirstname: firstname =>
+    module.exports
+      .bookings()
+      .find(booking => booking.element('div:nth-child(1) p').getText() === firstname),
 
   findBookingByBookingId: bookingId => {
-    return module.exports.bookings().find(booking => booking.getAttribute('id') === bookingId);
+    try {
+      const bookingDetails = module.exports
+        .bookings()
+        .find(booking => booking.getAttribute('id') === bookingId);
+      if (bookingDetails === undefined) {
+        return false;
+      }
+      return bookingDetails;
+    } catch (error) {
+      return console.log('error is:', error.message);
+    }
   },
 
   /**
@@ -59,5 +89,5 @@ module.exports = {
    */
   open: () => {
     browser.url('/');
-  }
-}
+  },
+};
